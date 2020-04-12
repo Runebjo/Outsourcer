c2aac60c1a853f5e7121d94159e31c201c286fc7
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-04-12 21:06:12 +0200

    started with outline

--- src/components/outlines..js
+++ src/components/outlines..js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Dropdown, Button, Menu, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 export const Outlines = () => {
+
+    function handleMenuClick() {
+        console.log("click");
+    }
+
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item key="1">
+                Unwritten
+          </Menu.Item>
+            <Menu.Item key="2">
+                Waiting Response
+          </Menu.Item>
+            <Menu.Item key="3">
+                In Progress
+          </Menu.Item>
+            <Menu.Item key="4">
+                Published
+          </Menu.Item>
+        </Menu>
+    );
+
     return (
         <div>
-            <h1>Outlines</h1>
+            <Space size="large">
+                <Dropdown overlay={menu}>
+                    <Button>
+                        Filter Status <DownOutlined />
+                    </Button>
+                </Dropdown>
+                <Button type="primary">Create Outline</Button>
+            </Space>
         </div>
     )
 }
