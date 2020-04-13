20911518aa7ee18c7426bf773199321920780797
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-04-13 14:37:48 +0200

    Added first draft on templates page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link, Route, useLocation } from 'react-router-dom';
 
 import './App.css';
 import { Outlines } from './components/outlines.';
@@ -10,33 +9,47 @@ import { Writers } from './components/writers';
 
 function App() {
   const { Header, Content, Footer } = Layout;
+  const { pathname } = useLocation();
+  let defaultKey = ['1'];
+
+  switch (pathname) {
+    case '/templates':
+      defaultKey = ['2']
+      break;
+    case '/writers':
+      defaultKey = ['3']
+      break;
+    default:
+      defaultKey = ['1']
+      break;
+  }
+
+
   return (
-    <Router>
-      <Layout>
-        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Link to="/">Outlines</Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/templates">Templates</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Link to="/writers">Writers</Link>
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-            <Route path="/" exact component={Outlines} />
-            <Route path="/templates" component={Templates} />
-            <Route path="/writers" component={Writers} />
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Bear Solutions ©2020</Footer>
-      </Layout>
-    </Router>
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={defaultKey}>
+          <Menu.Item key="1">
+            <Link to="/">Outlines</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/templates">Templates</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/writers">Writers</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+          <Route path="/" exact component={Outlines} />
+          <Route path="/templates" component={Templates} />
+          <Route path="/writers" component={Writers} />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Bear Solutions ©2020</Footer>
+    </Layout>
   );
 }
 
--- src/components/templates.js
+++ src/components/templates.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Table, Popconfirm, message } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 export const Templates = () => {
+
+    function createTemplate() {
+        console.log("Create Template");
+    }
+
+    function editTemplate(key) {
+        console.log("Edit outline", key);
+    }
+
+    function confirmDelete(key) {
+        const newData = data.filter(d => d.key !== key);
+        setData(newData);
+        message.success("Outline Deleted");
+    }
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            dataIndex: 'action',
+            key: 'action',
+            render: (text, record) => (
+                <span style={{ float: 'right' }}>
+                    <Button type="link" onClick={() => editTemplate(record.key)} icon={<EditOutlined />} />
+                    <Popconfirm
+                        title={`Are you sure you want to delete the template: "${record.name}?"`}
+                        onConfirm={() => confirmDelete(record.key)}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <Button type="link" icon={<DeleteOutlined />} />
+                    </Popconfirm>
+                </span >
+            )
+        }
+    ];
+
+    const [data, setData] = useState([
+        {
+            key: 1,
+            name: 'Response post'
+        },
+        {
+            key: 2,
+            name: 'vs post'
+        }
+    ]);
+
     return (
         <div>
-            <h1>Templates</h1>
+            <Button type="primary" onClick={createTemplate}>Create Template</Button>
+            <Table columns={columns} dataSource={data} style={{ marginTop: 25 }}></Table>
         </div>
     )
 }
